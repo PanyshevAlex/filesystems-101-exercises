f3bf40f00dccb1ab088c5f5d50d0124007b1308a
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2022-11-11 23:51:13 +0300

    Update solution.c

--- 06-ext2-walk-path/solution.c
+++ 06-ext2-walk-path/solution.c
@@ -222,13 +222,10 @@ int get_inode_by_path(int fd, int inode_number, char* path, const struct ext2_su
 	struct ext2_group_desc group_desc = {};
 
 	off_t gd_offset = SUPERBLOCK_OFFSET + sizeof(*sb) + (inode_number - 1) / sb->s_inodes_per_group * sizeof(group_desc);
-	ssize_t read_size = pread(fd, &group_desc, sizeof(group_desc), gd_offset);
-	if (read_size < 0)
-		return -errno;
+	pread(fd, &group_desc, sizeof(group_desc), gd_offset);
 
-	read_size = pread(fd, &inode, sizeof(inode), block_size * group_desc.bg_inode_table + (inode_number - 1) % sb->s_inodes_per_group * sb->s_inode_size);
-	if (read_size < 0)
-		return -errno;
+	pread(fd, &inode, sizeof(inode), block_size * group_desc.bg_inode_table + (inode_number - 1) % sb->s_inodes_per_group * sb->s_inode_size);
+
 	if ((inode_number == 2) && ((inode.i_mode & LINUX_S_IFDIR) == 0))
 		return -ENOTDIR;
 	char* next_path = strchr(path, '/');
