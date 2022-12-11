bacdf4373495663ea0ff2d265656d8627790ebf1
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2022-12-11 14:52:37 +0300

    add 12-grpc

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -6,6 +6,14 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+	"fs101ex/pkg/workgroup"
+    "log"
+    "net"
+    "sync"
+	"time"
+    hashpb "fs101ex/pkg/gen/hashsvc"
+    parhashpb "fs101ex/pkg/gen/parhashsvc"
 )
 
 type Config struct {
@@ -52,31 +60,131 @@ type Config struct {
 //
 // Both performance counters must be placed to Prometheus namespace "parhash".
 type Server struct {
-	conf Config
+    conf Config
 
-	sem *semaphore.Weighted
+    sem *semaphore.Weighted
+
+    mutex sync.Mutex
+    globalCounter int
+
+    stop context.CancelFunc
+    l    net.Listener
+    wg   sync.WaitGroup
+
+	nr_nr_requests     prometheus.Counter
+	subquery_durations *prometheus.HistogramVec
 }
 
 func New(conf Config) *Server {
-	return &Server{
-		conf: conf,
-		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
-	}
+    return &Server{
+        conf: conf,
+        sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+        globalCounter: 0,
+    }
 }
 
 func (s *Server) Start(ctx context.Context) (err error) {
-	defer func() { err = errors.Wrap(err, "Start()") }()
+    defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+    /* implement me */
+    ctx, s.stop = context.WithCancel(ctx)
 
-	return nil
+    s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+    if err != nil {
+        return err
+    }
+
+    srv := grpc.NewServer()
+    parhashpb.RegisterParallelHashSvcServer(srv, s)
+
+	s.nr_nr_requests = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "parhash",
+		Name:      "nr_requests",
+		Help: "Number of requests",
+	})
+	s.subquery_durations = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "parhash",
+		Name:      "subquery_durations",
+		Help: "Subquery durations",
+		Buckets:   prometheus.ExponentialBucketsRange(0.1, 10000, 24),
+	},
+		[]string{"backend"},
+	)
+	s.conf.Prom.MustRegister(s.nr_nr_requests)
+	s.conf.Prom.MustRegister(s.subquery_durations)
+
+    s.wg.Add(2)
+    go func() {
+        defer s.wg.Done()
+
+        srv.Serve(s.l)
+    }()
+    go func() {
+        defer s.wg.Done()
+
+        <-ctx.Done()
+        s.l.Close()
+    }()
+
+    return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+    /* implement me */
+    return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+    /* implement me */
+    s.stop()
+    s.wg.Wait()
 }
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (resp *parhashpb.ParHashResp, err error) {
+
+	s.nr_nr_requests.Inc()
+    client := make([]hashpb.HashSvcClient, len(s.conf.BackendAddrs))
+    wg := workgroup.New(workgroup.Config{Sem: s.sem})
+    hashes := make([][]byte, len(req.Data))
+
+    for i, back_addr := range s.conf.BackendAddrs {
+        connection, err := grpc.Dial(back_addr, grpc.WithInsecure())
+        if err != nil {
+            log.Fatalf("Couldn't connect to backend addr %s with error %s", s.conf.BackendAddrs[i], err)
+        }
+        defer connection.Close()
+        client[i] = hashpb.NewHashSvcClient(connection)
+    }
+
+    for i, buf := range req.Data {
+        buf_idx := i
+        curr_buf := buf
+        wg.Go(ctx, func(ctx context.Context) error {
+            s.mutex.Lock()
+            idx := s.globalCounter % len(client)
+            s.globalCounter++
+            hashReq := hashpb.HashReq{Data: curr_buf}
+            s.mutex.Unlock()
+			start := time.Now()
+            hash, err := client[idx].Hash(ctx, &hashReq)
+			end := time.Since(start)
+            if err != nil {
+                return err
+            }
+			s.subquery_durations.With(prometheus.Labels{"backend": s.conf.BackendAddrs[backend_nr]}).Observe(float64(duration.Microseconds()) / 1000)
+            s.mutex.Lock()
+            hashes[buf_idx] = hash.Hash
+            s.mutex.Unlock()
+
+            return nil;
+        })
+    }
+
+    err = wg.Wait()
+    if err != nil {
+        log.Fatalf("Error: can't hash given data with error %v", err)
+    }
+
+    return &parhashpb.ParHashResp{Hashes: hashes}, nil
+}
+
